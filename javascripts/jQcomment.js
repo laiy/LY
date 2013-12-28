b28e49ca1788cc3428b3c5893a19bd2fee318620
赖渊 <ly.franky@gmail.com>
2013-12-28 16:14:55 +0800

    Update jQcomment.js

--- javascripts/jQcomment.js
+++ javascripts/jQcomment.js
@@ -14,3 +14,15 @@ $("#comment-submit").click(function() {
     }
 
 });
+
+document.getElementById("comment-text").onkeydown = function (moz_ev) {
+    var ev = null;
+    if (window.event) {
+        ev = window.event;
+    } else {
+        ev = moz_ev;
+    }
+    if (ev != null && ev.ctrlKey && ev.keyCode == 13) {
+        document.getElementById("comment-submit").click();
+    }
+}
