a5492844e2cb824ef896777bbd6940d8f52e1172
LY <ly.franky@gmail.com>
2013-12-29 11:09:25 +0800

    Update jQcomment.js

--- javascripts/jQcomment.js
+++ javascripts/jQcomment.js
@@ -5,8 +5,11 @@
     $("#comment-submit").click(function() {
 
         var $commentList = $("#comment-text");
+        var avoidAttack = /<[^>]+>/gi;
         if ($commentList.val().length < 4) {
             alert("输入的内容少于4个字符，不允许发布。");
+        } else if (avoidAttack.test($commentList.val())) {
+            alert("发布内容有攻击嫌疑，不允许发布。")
         } else {
             $("#comments-list-ul").append("<li><p></p></li>");
             $("#comments-list-ul li:last-child").addClass("comment-item").children().append($commentList.val());
